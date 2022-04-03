7a3cfa068a13a30a6a3c5cce8f412d7f25bfec90
AlexInyaev <46852628+AlexInyaev@users.noreply.github.com>
2022-04-03 16:19:11 +0300

    castom selekt save

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const options = document.querySelectorAll('.option'),
     select = document.querySelector('.select'),
     selectHeader = document.querySelector('.selectHeader'),
     selectBody = document.querySelector('.selectBody'),
+    // scrollElem = document.querySelector('.scrollElem'),
+    // wrapperScroll = document.querySelector('.wrapperScroll'),
     wrapper = document.querySelector('.wrapper');
 
 let out = '';
@@ -19,6 +21,7 @@ options.forEach(item => {
 function openList() {
     selectHeader.style.display = 'none';
     selectBody.style.display = 'block';
+    // wrapperScroll.style.display = 'block';
     wrapper.classList.add('addBorder');
 }
 
@@ -26,6 +29,7 @@ function selectItem(item) {
     select.textContent = item.textContent;
     selectHeader.style.display = 'flex';
     selectBody.style.display = 'none';
+    // wrapperScroll.style.display = 'none';
     wrapper.classList.remove('addBorder');
 
 }
@@ -33,7 +37,23 @@ function selectItem(item) {
 function cursorLeaveWrapper() {
     selectHeader.style.display = 'flex';
     selectBody.style.display = 'none';
+    // wrapperScroll.style.display = 'none';
     wrapper.classList.remove('addBorder');
 }
 
 
+// 88888888888888888
+
+
+// selectBody.addEventListener('scroll', function () {
+//     // console.log(selectBody.scrollHeight);       //высота контента видимый + невидимый
+//     // console.log(selectBody.clientHeight);       // высота области содержимого вместе с внутренними отступами padding, но без прокрутки
+//     // console.log(selectBody.scrollTop);          // высота невидимой, прокрученной в данный момент, части элемента слева и сверху.
+//     // console.log(Math.round(selectBody.scrollTop / ((selectBody.scrollHeight - selectBody.clientHeight) / 45)))
+//     scrollElem.style.top = `${Math.round(selectBody.scrollTop / ((selectBody.scrollHeight - selectBody.clientHeight) / 45))}%` //расчет отступа ползунка от верха и добавление расчета в стили
+//     // console.log(scrollElem.style.top)
+// });
+
+// scrollElem.onmousedown = (e) => {
+//     console.log(e)
+// }
